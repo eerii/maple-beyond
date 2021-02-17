1be7c65ba834a31a6a515a7ae28551b621ae43ef
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2021-02-17 20:06:56 +0100

    Document 📄

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,19 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>MOOSE ~ Time Banking</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           Welcome to <a>MOOSE!</a>
         </h1>
       </main>
 
-      <footer className={styles.footer}>
+      <footer>
         <p>MOOSE, 2021</p>
       </footer>
     </div>
