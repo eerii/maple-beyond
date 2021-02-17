e4c8874991586a4bf909b35b3e5d05ab457779b2
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2021-02-17 20:51:05 +0100

    Matomo Analytics 📈

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from 'next/app'
+import { init } from "@socialgouv/matomo-next"
+import { useEffect } from 'react'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+
+const App = ({ Component, pageProps }: AppProps) => {
+	useEffect(() => {
+		init({
+			url: MATOMO_URL,
+			siteId: MATOMO_SITE_ID
+		})
+	}, [])
+
+	return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
